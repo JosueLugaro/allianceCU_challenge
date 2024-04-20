643bd3a8e47d766433c875d5e7b4baa043e63fdb
Josue Lugaro <josuelugaro15@gmail.com>
2024-04-20 14:33:07 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.46"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e001c9271cf7f3b9"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "AllianceCUServerInstance"
+  }
+}
+
