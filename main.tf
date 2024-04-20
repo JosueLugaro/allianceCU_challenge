21f83b241720251d23dc3f7398a6fe82893b40d8
Josue Lugaro <josuelugaro15@gmail.com>
2024-04-20 17:16:07 -0500

    Created role to give ec2 instance s3 write access

--- main.tf
+++ main.tf
@@ -13,9 +13,45 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data aws_iam_policy_document "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type	  = "Service"
+      identifiers = ["ec2.amazonaws.com"] 
+    }
+  }
+}
+
+data aws_iam_policy_document "s3_write_access" {
+  statement {
+    actions = ["s3:PutObject"] 
+    resources = ["arn:aws:s3:::allianceCU_challenge_s3"]
+  }
+}
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "ec2_iam_role"
+  assume_policy_role = "${data.aws_iam_policy_document.ec2_assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "join_policy" {
+  depends_on = ["aws_iam_role.ec2_iam_role"]
+  name       = "join_policy"
+  role       = "${aws_iam_role.ec2_iam_role.name}"
+  policy     = "${data.aws_iam_policy_document.s3_write_access.json}"
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "instance_profile"
+  role = "${aws_iam_role_policy.join_policy.name}"
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0e001c9271cf7f3b9"
   instance_type = "t3.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.instance_profile}"
 
   tags = {
     Name = "AllianceCUServerInstance"
